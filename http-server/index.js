5d87d241c73a39c1a9ca5ab960770981ffbdfb38
vinit <vinitmay17@gmail.com>
2023-03-25 16:16:43 +0530

    retry level2

--- http-server/index.js
+++ http-server/index.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const fs = require("fs");
 
-const homeContent = fs.readFileSync("home.html");
-const projectContent = fs.readFileSync("project.html");
-const registrationContent = fs.readFileSync("registration.html");
+const homeContent = fs.readFileSync("./home.html", "utf-8"); 
+const projectContent = fs.readFileSync("./project.html", "utf-8"); 
+const registrationContent = fs.readFileSync("./registration.html", "utf-8"); 
 
 const args = process.argv.slice(2);
 const port = args.includes('--port') ? parseInt(args[args.indexOf('--port') + 1]) : 5000;
@@ -13,13 +13,13 @@ http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/html" });
   switch (url) {
     case "/project":
-      response.write(projectContent);
+      response.write(projectContent.replace(/href="\/registration"/g, 'href="/registration"')); 
       break;
     case "/registration":
       response.write(registrationContent);
       break;
     default:
-      response.write(homeContent);
+      response.write(homeContent.replace(/href="\/project"/g, 'href="./project"')); 
       break;
   }
   response.end();
